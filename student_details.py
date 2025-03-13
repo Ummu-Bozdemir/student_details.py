23492829037419f16273a908556d45baa5b01175
Ummu-Bozdemir <ummubdemir@gmail.com>
2025-03-13 10:29:11 +0100

    Create student_details.py

---
+++ student_details.py
@@ -0,0 +1,14 @@
+first_name = "Ummu"
+last_name = "Bozdemir"
+index_number = "35449"
+nationalitr = "Turkish"
+strarting_date = "2003-09-15"
+courses = ["Computer Scince"]
+
+print("Student Details:")
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Index Number:", index_number)
+print("Nationality:", nationality)
+print("Starting Date:", starting_date)
+print("Courses:", courses)
